c4c44152b452bd690396c3b81ca69d5bff2b64cf
MollenFerneus <osgous1@gmail.com>
2024-09-12 18:28:32 +0200

    Update Data wrangling - duplicates fix.py

--- Data wrangling - duplicates fix.py
+++ Data wrangling - duplicates fix.py
@@ -5,23 +5,24 @@ print(df.head())
 
 duplicates = df.duplicated()            #returns bool if duplicates appear, true for duplicate and false for unique
 
-print(duplicates.sum())                 #prints the of sums the true values (duplicates) and returns the amount of duplicates
+print(df["Respondent"].duplicated().sum())
+
+print("\nDuplicates: ", duplicates.sum())                 #prints the of sums the true values (duplicates) and returns the amount of duplicates
 
 df = df.drop_duplicates()      #deletes the duplicates and saves the dataframe under new variable
 
-print(df.duplicated().sum())       #prints the sum of duplicates in new dataframe to verify if the duplicates were deleted
+print("\nDuplicates after deletion:", df.duplicated().sum())       #prints the sum of duplicates in new dataframe to verify if the duplicates were deleted
 
-print(df.isna().sum().sum())          #prints the missing values, then sums them by the column, then sums the columns together and prints the total of amount of missing values
+print("\nMissing values: ",df.isna().sum().sum())          #prints the missing values, then sums them by the column, then sums the columns together and prints the total of amount of missing values
 
-print(df["WorkLoc"].isna().sum())      #prints the sum of misisng values in specific column called "WorkLoc"
+print("\nMissing values in WorkLoc: ",df["WorkLoc"].isna().sum())      #prints the sum of misisng values in specific column called "WorkLoc"
 
-print(df["WorkLoc"].value_counts())        #prints the value_counts which is the sum of the data that is being repeated, for instance "Office" was in 6806 positions
+print("\nRepeated values and the count: ",df["WorkLoc"].value_counts())        #prints the value_counts which is the sum of the data that is being repeated, for instance "Office" was in 6806 positions
 
-print(df["WorkLoc"].value_counts().max())          #prints the most repeated as a count/amount
-print(df["WorkLoc"].value_counts().idxmax())        #prints the most repeated word/value
+print("\nMost repeated value count: ", df["WorkLoc"].value_counts().max())          #prints the most repeated as a count/amount
+print("\nMost repeated value: ",df["WorkLoc"].value_counts().idxmax())        #prints the most repeated word/value
 
 m_value = df["WorkLoc"].value_counts().idxmax()         #saves the value of most repeated value as a variable
-print(m_value)
 
 df["WorkLoc"] = df["WorkLoc"].fillna(m_value)           #fills the empty data in "WorkLoc" column with the most repeated value
-print(df["WorkLoc"].isna().sum())                       #prints the empty places in "WorkLoc" column (should be 0)
+print("\nEmpty places after filling data: ", df["WorkLoc"].isna().sum())                       #prints the empty places in "WorkLoc" column (should be 0)
